844aa3a51716bf840bb1671ee19e19215650935f
knopfler81 <knopfler42@yahoo.fr>
2016-08-21 00:46:14 +0200

    admin

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -8,12 +8,12 @@ module Admin
   class ApplicationController < Administrate::ApplicationController
     before_filter :authenticate_admin
 
-
+    if Rails.env.production?
       http_basic_authenticate_with name: ENV["ADMIN_NAME"], password: ENV["ADMIN_PASSWORD"]
-
-
+    else
       # http_basic_authenticate_with name: ENV.fetch("ADMIN_NAME"), password: ENV.fetch("ADMIN_PASSWORD")
-      #http_basic_authenticate_with name: "admin", password: "123456"
+    http_basic_authenticate_with name: "admin", password: "123456"
+  end
 
 
     def authenticate_admin
