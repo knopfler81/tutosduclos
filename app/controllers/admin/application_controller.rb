a23b352eeae00138870229713c411024d3ba159b
knopfler81 <knopfler42@yahoo.fr>
2016-08-20 23:28:23 +0200

    modif

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -7,7 +7,9 @@
 module Admin
   class ApplicationController < Administrate::ApplicationController
     before_filter :authenticate_admin
+
     http_basic_authenticate_with name: "admin", password: "123456"
+    
     def authenticate_admin
       # TODO Add authentication logic here.
     end
