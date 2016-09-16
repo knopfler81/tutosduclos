302c59c95b9530b0faaa24d4e09176a4438208de
knopfler81 <knopfler42@yahoo.fr>
2016-09-16 14:14:57 +0200

    authenticate admin

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,11 +1,14 @@
 class Admin::DashboardController < ApplicationController
-
-  if Rails.env.production?
-    http_basic_authenticate_with :name => ENV["ADMIN_NAME"],:password => ENV["ADMIN_PASSWORD"]
-  else
-    http_basic_authenticate_with :name => "admin", :password => "password"
-  end
+  # before_action :authenticate_user!
+  # if Rails.env.production?
+  #   http_basic_authenticate_with :name => ENV["ADMIN_NAME"],:password => ENV["ADMIN_PASSWORD"]
+  # else
+  #   http_basic_authenticate_with :name => "admin", :password => "password"
+  # end
 
   def index
+    if current_user.admin !=  true
+      redirect_to tutos_path
+    end
   end
 end
