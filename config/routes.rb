fd543dbe01db3c454ec2a255208e08eb2782bc0b
knopfler81 <knopfler42@yahoo.fr>
2016-09-21 16:20:12 +0200

    fixing user_path

--- config/routes.rb
+++ config/routes.rb
@@ -4,31 +4,26 @@ Rails.application.routes.draw do
     resources :tutos
   end
 
+  resources :tutos, only: [:show]
+
   resources :tutos do
     member do
       put "like", to: "tutos#upvote"
     end
   end
 
-as :user do
-  get '/register', to: 'devise/registrations#new', as: :register
-  get '/login',    to: 'devise/sessions#new', as: :login
-  get '/logout',   to: 'devise/sessions#destroy', as: :logout
-end
+  get     "/register",  to: "devise/registrations#new", as: :register
+  get     "/login",     to: "devise/sessions#new", as: :login
+  get     "/logout",    to: "devise/sessions#destroy", as: :logout
+  get     "/account",   to: "users#show", as: :account
+  get     "/login" ,    to: "devise/sessions#new", as: :new_user_session
+  post    "/login" ,    to: "devise/sessions#create", as: :user_session
+  delete  "/logout" ,   to: "devise/sessions#destroy", as: :destroy_user_session
 
-devise_for :users, skip: [:sessions]
-
-as :user do
-  get     "/account"  => "users#show", as: :account
-  get     "/login"    => "devise/sessions#new", as: :new_user_session
-  post    "/login"    => "devise/sessions#create", as: :user_session
-  delete  "/logout"   => "devise/sessions#destroy", as: :destroy_user_session
-end
+  devise_for :users, skip: [:sessions]
 
   resources :users
-  resources :tutos
 
   root "home#landing"
-
 
 end
