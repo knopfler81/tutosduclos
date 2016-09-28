9cdeafc91e4647e486317b9e980c687d7b91ef4e
knopfler81 <knopfler42@yahoo.fr>
2016-09-29 01:03:22 +0200

    users routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     end
   end
 
+as :user do
   get     "/register",  to: "devise/registrations#new", as: :register
   get     "/login",     to: "devise/sessions#new", as: :login
   get     "/logout",    to: "devise/sessions#destroy", as: :logout
@@ -19,7 +20,7 @@ Rails.application.routes.draw do
   get     "/login" ,    to: "devise/sessions#new", as: :new_user_session
   post    "/login" ,    to: "devise/sessions#create", as: :user_session
   delete  "/logout" ,   to: "devise/sessions#destroy", as: :destroy_user_session
-
+end
   devise_for :users, skip: [:sessions]
 
   resources :users
