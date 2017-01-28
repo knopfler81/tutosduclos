a2e27803c106051a5d1adf6de75d83d2cddd041e
Nelly Duclos <knopfler42@yahoo.fr>
2017-01-29 00:00:42 +0100

    added access to avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :id)
+    params.require(:user).permit( :avatar, :first_name, :last_name, :email, :id)
   end
 end
