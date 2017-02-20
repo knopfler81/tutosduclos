dbeb55150ea6c2812a6b87767b84ccd8623e75db
Nelly Duclos <knopfler42@yahoo.fr>
2017-02-20 23:00:49 +0100

    seting facebook auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,26 @@
 Rails.application.routes.draw do
 
-  scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
-    get "/best_voted", to: "tutos#best_voted"
+  #scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
+  root "home#landing"
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  get "/best_voted", to: "tutos#best_voted"
+  resources :tutos
+  namespace :users do
     resources :tutos
-    namespace :users do
-      resources :tutos
-    end
-
-    resources :tutos, only: [:show]
-
-    resources :tutos do
-      member do
-        put "like", to: "tutos#upvote"
-      end
-    end
-
-  as :user do
-    get     "/register",  to: "devise/registrations#new", as: :register
-    get     "/login",     to: "devise/sessions#new", as: :login
-    get     "/logout",    to: "devise/sessions#destroy", as: :logout
-    get     "/account",   to: "users#show", as: :account
-    get     "/login" ,    to: "devise/sessions#new", as: :new_user_session
-    post    "/login" ,    to: "devise/sessions#create", as: :user_session
-    delete  "/logout" ,   to: "devise/sessions#destroy", as: :destroy_user_session
   end
 
-    devise_for :users, skip: [:sessions]
+  resources :tutos, only: [:show]
 
-    resources :users
-
-    root "home#landing"
+  resources :tutos do
+    member do
+      put "like", to: "tutos#upvote"
+    end
   end
-  get '*path', to: redirect("/#{I18n.default_locale}/%{path}")
-  get '', to: redirect("/#{I18n.default_locale}")
+
+
+
+  #end
+  # get '*path', to: redirect("/#{I18n.default_locale}/%{path}")
+  # get '', to: redirect("/#{I18n.default_locale}")
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170128214443) do
+ActiveRecord::Schema.define(version: 20170220210650) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -45,14 +45,19 @@ ActiveRecord::Schema.define(version: 20170128214443) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "first_name"
-    t.string   "last_name"
     t.boolean  "admin"
     t.string   "nickname"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "facebook_picture_url"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "token"
+    t.datetime "token_expiry"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
