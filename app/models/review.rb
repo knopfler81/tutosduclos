957e9d9ec70468bf483d614ada401ffa495ef2ed
Nelly Duclos <knopfler42@yahoo.fr>
2017-03-30 01:16:17 +0200

    remove faling validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ActiveRecord::Base
   belongs_to :tuto
   belongs_to :user
   validates :content, presence: true
-  validates :rating, presence: true
+  #validates :rating, presence: true
   scope :by_date, -> { order(created_at: :desc) }
 end
