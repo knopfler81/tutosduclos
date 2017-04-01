af77fd66323110bb2b91ff6aa7b0370b76b89c09
Nelly <knopfler42@yahoo.fr>
2017-04-01 12:14:51 +0200

    Users access (#61)
    * fixed avatar
    
    * user must be loged in and have complete profile to leave comment

--- db/migrate/20170331155131_remove_another_avatar_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAnotherAvatarFromUsers < ActiveRecord::Migration
-  def change
-    remove_column :users, :avatar_updated_at
-  end
-end
--- db/migrate/20170331155315_remove_avatar_size_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAvatarSizeFromUsers < ActiveRecord::Migration
-  def change
-    remove_column :users, :avatar_file_size
-  end
-end
