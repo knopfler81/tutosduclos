ba901e56b965590090620b4aa329fc6816048be9
Nelly Duclos <knopfler42@yahoo.fr>
2017-04-07 12:41:35 +0200

    added sendgrid conf

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,9 @@
-ActionMailer::Base.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: 'gmail.com',
-  user_name: ENV['GMAIL_ADDRESS'],
-  password: ENV['GMAIL_APP_PASSWORD'],
-  authentication: :login,
-  enable_starttls_auto: true
+  ActionMailer::Base.smtp_settings = {
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => "heroku.com",
+  :enable_strattls_auto => true
 }
